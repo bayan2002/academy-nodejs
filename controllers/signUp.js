6413089105c64ba73c537582bd03104c6c4b8d80
shathakh <shathaqudaih17@gmail.com>
2023-02-23 13:44:05 +0200

    edit signup api for teacher

--- controllers/signUp.js
+++ controllers/signUp.js
@@ -1,69 +1,115 @@
-const { Teacher } = require('../models');
-const validateTeacherEmail = require('../validation')
-const {serverErrs} = require('../middlewares/customError');
-const generateRandomCode = require('../middlewares/generateCode');
-const sendEmail = require('../middlewares/sendEmail');
-const { compare, hash } = require('bcrypt');
-const generateToken = require('../middlewares/generateToken');
+const { Teacher, Student, Parent } = require("../models");
+const validateTeacherEmail = require("../validation");
+const { serverErrs } = require("../middlewares/customError");
+const generateRandomCode = require("../middlewares/generateCode");
+const sendEmail = require("../middlewares/sendEmail");
+const { compare, hash } = require("bcrypt");
+const generateToken = require("../middlewares/generateToken");
 
+const signUp = async (req, res) => {
+  const { email } = req.body;
+  await validateTeacherEmail.validate({ email });
 
-const signUp = async(req, res)=> {
-const {email} = req.body;
-await validateTeacherEmail.validate({email});
-const teacher = await Teacher.findOne({
-  where: {
+  const teacher = await Teacher.findOne({
+    where: {
+      email,
+      isRegistered: true,
+    },
+  });
+
+  const student = await Student.findOne({
+    where: {
+      email,
+    },
+  });
+   
+  const parent = await Parent.findOne({
+    where: {
+      email,
+    },
+  });
+
+  if (teacher) throw serverErrs.BAD_REQUEST("email is already used");
+  if (student) throw serverErrs.BAD_REQUEST("email is already used");
+  if (parent) throw serverErrs.BAD_REQUEST("email is already used");
+
+  const code = generateRandomCode();
+  const newTeacher = await Teacher.create({
     email,
-    isRegister: true
-  },
-});
-if (teacher) throw serverErrs.BAD_REQUEST('email is already used');
+    registerCode: code,
+  });
+  await newTeacher.save();
 
- const code = generateRandomCode();
-const newTeacher = await Teacher.create({
-  email,
-  registerCode: code
-})
- await newTeacher.save();
+  sendEmail(email, code);
+};
 
- sendEmail(email, code);
-}
+const verifyCode = async (req, res) => {
+  const { registerCode, email } = req.body;
 
-const verifyCode = async(req, res)=> {
-const {registerCode, email} = req.body;
+  const teacher = await Teacher.findOne({
+    where: {
+      email,
+    },
+  });
 
-const teacher = await Teacher.findOne({
-  where: {
-    email,
-  },
-});
-if (!teacher) throw serverErrs.BAD_REQUEST('email not found');
-if(teacher.isRegister) throw serverErrs.BAD_REQUEST('email is already used');
-if(teacher.registerCode !== registerCode) throw serverErrs.BAD_REQUEST('code is wrong')
+  const student = await Student.findOne({
+    where: {
+      email,
+    },
+  });
 
-await teacher.update({isRegister : true})
- 
-}
+  const parent = await Parent.findOne({
+    where: {
+      email,
+    },
+  });
 
-const signPassword = async(req, res)=> {
-const {email, password} = req.body;
+  if (!teacher) throw serverErrs.BAD_REQUEST("email not found");
+  if (teacher.isRegistered)
+    throw serverErrs.BAD_REQUEST("email is already used");
+  if (student) throw serverErrs.BAD_REQUEST("email is already used");
+  if (parent) throw serverErrs.BAD_REQUEST("email is already used");
+  if (teacher.registerCode !== registerCode)
+    throw serverErrs.BAD_REQUEST("code is wrong");
 
-const teacher = await Teacher.findOne({
-  where: {
-    email,
-  },
-});
-if (!teacher) throw serverErrs.BAD_REQUEST('email not found');
-if(teacher.isRegister) throw serverErrs.BAD_REQUEST('email is already used');
-const hashedPassword = await hash(password, 12);
+  await teacher.update({ isRegister: true });
+};
 
-await teacher.update({password: hashedPassword});
-await teacher.save();
+const signPassword = async (req, res) => {
+  const { email, password } = req.body;
 
-const token = await generateToken({ userId: teacher.id});
+  const teacher = await Teacher.findOne({
+    where: {
+      email,
+    },
+  });
 
-  res.cookie('token', token);
-  res.send({ status: 201, data: teacher, msg: 'successful sign up'});
+  const student = await Student.findOne({
+    where: {
+      email,
+    },
+  });
 
-}
+  const parent = await Parent.findOne({
+    where: {
+      email,
+    },
+  });
 
-module.exports = {signUp, verifyCode, signPassword};
+  if (!teacher) throw serverErrs.BAD_REQUEST("email not found");
+  if (teacher.isRegister) throw serverErrs.BAD_REQUEST("email is already used");
+  if (student) throw serverErrs.BAD_REQUEST("email is already used");
+  if (parent) throw serverErrs.BAD_REQUEST("email is already used");
+
+  const hashedPassword = await hash(password, 12);
+
+  await teacher.update({ password: hashedPassword });
+  await teacher.save();
+
+  const token = await generateToken({ userId: teacher.id });
+
+  res.cookie("token", token);
+  res.send({ status: 201, data: teacher, msg: "successful sign up" });
+};
+
+module.exports = { signUp, verifyCode, signPassword };
