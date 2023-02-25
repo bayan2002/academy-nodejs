fc78538373256b83f35ad1eb62b91624cd7f44ac
shathakh <shathaqudaih17@gmail.com>
2023-02-25 23:30:07 +0200

    add fake data

--- db/config/fakeData.js
+++ db/config/fakeData.js
@@ -5,6 +5,7 @@ const admin = [
     password: "$2y$10$ZM/WrL9nOvwvpFMrF9TULuJgQfc1F2HpqVh9oyGDzyn1xqH4gkPTO", // 1234
   },
 ];
+
 const teachers = [
   {
     email: "ali@gmail.com",
@@ -30,7 +31,9 @@ const teachers = [
     instantBooking: true,
     isRegistered: false,
     isVerified: false,
-
+    EducationDegreeId: 1,
+    CertificateId: 1,
+    ExperienceId: 1,
     // 1
   },
   {
@@ -57,7 +60,9 @@ const teachers = [
     instantBooking: true,
     isRegistered: false,
     isVerified: false,
-
+    EducationDegreeId: 2,
+    CertificateId: 2,
+    ExperienceId: 2,
     // 2
   },
   {
@@ -84,9 +89,13 @@ const teachers = [
     instantBooking: true,
     isRegistered: false,
     isVerified: false,
+    EducationDegreeId: 3,
+    CertificateId: 2,
+    ExperienceId: 3,
     // 3
   },
 ];
+
 const students = [
   {
     email: "rawan@gmail.com",
@@ -102,6 +111,9 @@ const students = [
     regionTime: "GM",
     // image:
     //   "https://www.shutterstock.com/image-photo/head-shot-profile-photo-portrait-260nw-1831529317.jpg",
+    LevelId: 1,
+    ClassId: 1,
+    CurriculumId: 1,
     // 1
   },
   {
@@ -118,6 +130,9 @@ const students = [
     regionTime: "GM",
     // image:
     //   "https://www.shutterstock.com/image-photo/head-shot-profile-photo-portrait-260nw-1831529317.jpg",
+    LevelId: 2,
+    ClassId: 2,
+    CurriculumId: 2,
     // 2
   },
   {
@@ -134,9 +149,13 @@ const students = [
     regionTime: "GM",
     // image:
     //   "https://www.shutterstock.com/image-photo/head-shot-profile-photo-portrait-260nw-1831529317.jpg",
+    LevelId: 3,
+    ClassId: 3,
+    CurriculumId: 3,
     // 3
   },
 ];
+
 const subjectCategories = [
   {
     titleAR: "رياضيات",
@@ -155,6 +174,7 @@ const subjectCategories = [
     titleEN: "history",
   }, //4
 ];
+
 const subjects = [
   {
     titleAR: "جبر",
@@ -187,6 +207,7 @@ const subjects = [
     subjectCategoryId: 3,
   }, //6
 ];
+
 const levels = [
   {
     titleAR: "لبتدائي",
@@ -205,6 +226,117 @@ const levels = [
     titleEN: "University",
   }, //4
 ];
+
+const classes = [
+  {
+    titleAR: "سنة أولى",
+    titleEN: "first year",
+  }, //1
+  {
+    titleAR: "سنة ثانية",
+    titleEN: "second year",
+  }, //2
+  {
+    titleAR: "سنة ثالثة",
+    titleEN: "third year",
+  }, //3
+  {
+    titleAR: "سنة رابعة",
+    titleEN: "fourth year",
+  }, //4
+];
+
+const curriculums = [
+  {
+    titleAR: " المنهج الأمريكي",
+    titleEN: "american curriculum",
+  }, //1
+  {
+    titleAR: "المنهج البريطاني",
+    titleEN: "british curriculum",
+  }, //2
+  {
+    titleAR: "المنهج السعودي",
+    titleEN: "saudi curriculum",
+  }, //3
+  {
+    titleAR: " المنهج الاماراتي",
+    titleEN: "emarites curriculum",
+  }, //4
+];
+
+const languages = [
+  {
+    title: "english"
+  },
+  {
+    title: "arabic"
+  },
+  {
+    title: "french"
+  },
+  {
+    title: "italian"
+  }
+]
+
+const experiences = [
+  {
+    jobTitle : "arabic teacher",
+    companyName: "islam school",
+    from: "12/4/2010",
+    to: "8/8/2020"
+  },
+  {
+    jobTitle : "english teacher",
+    companyName: "american school",
+    from: "3/3/3303",
+    to: "4/5/5505"
+  },
+  {
+    jobTitle : "math teacher",
+    companyName: "ahmed school",
+    from: "2/4/2003",
+    to: "2/5/2023"
+  }
+]
+
+const educationDegree = [
+  {
+    UniversityName: "azhar university", 
+    degree: "BA",
+    from: "23/5/2000",
+    to: "23/5/2007"
+  },
+  {
+    UniversityName: "islamic university", 
+    degree: "master",
+    from: "30/3/2020",
+    to: "3/4/2022"
+  },
+  {
+    UniversityName: "gaza university", 
+    degree: "BA",
+    from: "22/3/1987",
+    to: "5/6/1988"
+  }
+]
+
+const certificates = [
+  {
+    name: "web development",
+    subject: "technology",
+    from: "22/3/1987",
+    to: "5/6/1988"
+  },
+  {
+    name: "algorethims",
+    subject: "math",
+    from: "22/3/1987",
+    to: "5/6/1988"
+  }
+]
+
 const parents = [
   {
     email: "omar@gmail.com",
