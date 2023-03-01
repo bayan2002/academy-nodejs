054030f45cf8f0373cf83ce1a9a1ebe8ada5f5fa
bayan2002 <bayanabdelbaryq@gmail.com>
2023-03-01 12:34:58 +0200

    make errorCatcher workes for allcontrollers

--- routes/student.js
+++ routes/student.js
@@ -2,10 +2,11 @@ const express = require("express");
 
 const studentRouter = express.Router();
 const {signUp, verifyCode, signPassword, signData} = require("../controllers/student");
+const errorCatcher = require("../middlewares/errorCatcher");
 
-studentRouter.post("/signup", signUp);
-studentRouter.post("/signup/code", verifyCode);
-studentRouter.post("/signup/pass", signPassword);
-studentRouter.post("/signup/data", signData);
+studentRouter.post("/signup", errorCatcher(signUp));
+studentRouter.post("/signup/code", errorCatcher(verifyCode));
+studentRouter.post("/signup/pass", errorCatcher(signPassword));
+studentRouter.post("/signup/data", errorCatcher(signData));
 
 module.exports = studentRouter;
