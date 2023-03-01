3d9f8f5b7819e7ac312279202c4b925baa0be686
bayan2002 <bayanabdelbaryq@gmail.com>
2023-03-01 10:48:20 +0200

    edit data type for student model

--- db/config/build.js
+++ db/config/build.js
@@ -32,19 +32,19 @@ const {
 
 const insertDB = async () => {
   await sequelize.sync({ force: true });
-  await Admin.bulkCreate(admin);
-  await Student.bulkCreate(students);
-  await Parent.bulkCreate(parents);
-  await Teacher.bulkCreate(teachers);
-  await SubjectCategory.bulkCreate(subjectCategories);
-  await Subject.bulkCreate(subjects);
-  await Level.bulkCreate(levels);
-  await Class.bulkCreate(classes);
-  await Curriculum.bulkCreate(curriculums);
-  await Language.bulkCreate(languages);
-  await Experience.bulkCreate(experiences);
-  await EducationDegree.bulkCreate(educationDegree);
-  await Certificates.bulkCreate(certificates);
+  await Admin.bulkCreate(admin).then(() => console.log("Admin data have been saved"));
+  await Parent.bulkCreate(parents).then(() => console.log("Parent data have been saved"));
+  await Teacher.bulkCreate(teachers).then(() => console.log("Teacher data have been saved"));
+  await SubjectCategory.bulkCreate(subjectCategories).then(() => console.log("subCategory data have been saved"));
+  await Subject.bulkCreate(subjects).then(() => console.log("Subject data have been saved"));
+  await Level.bulkCreate(levels).then(() => console.log("level data have been saved"));
+  await Class.bulkCreate(classes).then(() => console.log("Class data have been saved"));
+  // await Curriculum.bulkCreate(curriculums);
+  // await Student.bulkCreate(students).then(() => console.log("student data have been saved"));
+  // await Language.bulkCreate(languages);
+  // await Experience.bulkCreate(experiences);
+  // await EducationDegree.bulkCreate(educationDegree);
+  // await Certificates.bulkCreate(certificates);
 };
 if (process.env.SEED) {
   insertDB();
