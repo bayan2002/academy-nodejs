4c64cdf7a2229a9c963d3ea66db01dde3e66c686
bayan2002 <bayanabdelbaryq@gmail.com>
2023-03-01 10:32:05 +0200

    make the admin controllers workes in the rout

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,54 @@
 const express = require("express");
 
 const adminRouter = express.Router();
-const { login, signUp, getLevels, getSubjectCategories, getSingleSubjectCategory, getSubjects, getSingleSubject, getClasses,
-  getSingleClass, getSingleLevel, getCurriculums, getSingleCurriculum  } = require("../controllers/admin");
-const logout = require("../middlewares/logout")
+const {
+  login,
+  signUp,
+  getLevels,
+  getSubjectCategories,
+  getSingleSubjectCategory,
+  getSubjects,
+  getSingleSubject,
+  getClasses,
+  getSingleClass,
+  getSingleLevel,
+  getCurriculums,
+  getSingleCurriculum,
+  createSubjectCategory,
+  createSubject,
+  createLevel,
+  createClass,
+  createCurriculum,
+} = require("../controllers/admin");
+const checkUserAuth = require("../middlewares/checkUserAuth");
+const logout = require("../middlewares/logout");
+const verifyToken = require("../middlewares/verifyToken");
 
 adminRouter.post("/signup", signUp);
 adminRouter.post("/login", login);
 adminRouter.post("/logout", logout);
-adminRouter.get("/get", getLevels );
+
+adminRouter.post(
+  "/subjectCategory",
+  verifyToken,
+  checkUserAuth("admin"),
+  createSubjectCategory
+);
+adminRouter.post(
+  "/subject",
+  verifyToken,
+  checkUserAuth("admin"),
+  createSubject
+);
+adminRouter.post("/level", verifyToken, checkUserAuth("admin"), createLevel);
+adminRouter.post("/class", verifyToken, checkUserAuth("admin"), createClass);
+adminRouter.post(
+  "/curriculum",
+  verifyToken,
+  checkUserAuth("admin"),
+  createCurriculum
+);
+
 adminRouter.get("/subCategories", getSubjectCategories);
 adminRouter.get("/subCategory", getSingleSubjectCategory);
 adminRouter.get("/subjects", getSubjects);
