5057faba6274bf1129a658815a196250b2f0e971
shathakh <shathaqudaih17@gmail.com>
2023-03-02 11:03:17 +0200

    edit connection file

--- db/config/connection.js
+++ db/config/connection.js
@@ -4,36 +4,12 @@ const { Sequelize } = require("sequelize");
 dotenv.config();
 const { NODE_ENV, DATABASE_URL, DEV_DATABASE_URL } = process.env;
 
-// let url;
-// let ssl = false;
-// switch (NODE_ENV) {
-//   case "development":
-//     url = DEV_DATABASE_URL;
-//     break;
-//   case "production":
-//     url = DATABASE_URL;
-//     ssl = {
-//       rejectUnauthorized: false,
-//     };
-//     break;
-//   default:
-//     throw new Error("NODE_ENV is not set");
-// }
-
-// if (!url) throw new Error("NODE_ENV is not set");
-const sequelize = new Sequelize(DEV_DATABASE_URL, {
-  logging: false,
-  dialect: "postgres",
-});
-// const sequelize = new Sequelize("academy", "root", "root", {
+// const sequelize = new Sequelize(DEV_DATABASE_URL, {
 //   logging: false,
-//   dialect: "mysql",
-//   dialectOptions: {
-//     ssl,
-//   },
+//   dialect: "postgres",
 // });
-// const sequelize = new Sequelize("rescteeh_amman_db", "rescteeh_root", "059283805928388", {
-//   dialect:'mysql',host:"localhost"
-// });
-module.exports = sequelize;
 
+const sequelize = new Sequelize("rescteeh_amman_db", "rescteeh_root", "059283805928388", {
+  dialect:'mysql',host:"localhost"
+});
+module.exports = sequelize;
