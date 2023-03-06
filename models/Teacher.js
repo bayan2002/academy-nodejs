10895107e4e9aae4fb4d7c9bbb2ad58fff05dd83
bayan2002 <bayanabdelbaryq@gmail.com>
2023-03-06 19:24:00 +0200

    add timeZone attribute to teacher model

--- models/Teacher.js
+++ models/Teacher.js
@@ -57,6 +57,9 @@ const Teacher = Sequelize.define('Teacher', {
   favHours: {
     type: DataTypes.STRING,
   },
+  timeZone: {
+    type: DataTypes.STRING,
+  },
   shortHeadlineAr: {
     type: DataTypes.STRING,
   },
