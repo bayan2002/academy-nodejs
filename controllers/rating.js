3e51a35617cfbcedd75f7c16a6f18fa73c5f2b65
shathakh <shathaqudaih17@gmail.com>
2023-03-29 22:27:14 +0300

    create get teacher rate function

--- controllers/rating.js
+++ controllers/rating.js
@@ -1,6 +1,6 @@
 const Rate = require("../models/Rates");
 
-const rateTeacher = async()=> {
+const rateTeacher = async(req,res)=> {
 const {studentId, TeacherId, rating, comment} = req.body;
 
 const rate = await Rate.create({
@@ -12,6 +12,17 @@ res.send({
   data: rate,
   msg: "successful rate teacher",
 });
+
+}
+
+const getTeacherRate = async(req,res) => {
+  const {teacherId} = req.body
+const rates = await Rate.findAll({
+  where:{
+    TeacherId : teacherId
+  }
+})
+
 
 }
 
