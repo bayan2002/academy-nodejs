c9360324faf552aa633903e5d8fb0d1050713df6
bayan2002 <bayanabdelbaryq@gmail.com>
2023-03-29 13:49:56 +0200

    add mode: no_cors for feach data

--- controllers/payment.js
+++ controllers/payment.js
@@ -26,6 +26,7 @@ const charge = async () => {
     body: `{"client_reference_id":"123412","mode":"payment","products":[{"name":"product 1","quantity":1,"unit_amount":${
       newPrice * 1000
     }}],"success_url":"https://acacdemy.vercel.app/success-charge","cancel_url":"https://acacdemy.vercel.app/fail-charge","metadata":{"Customer name":"somename","order id":0}}`,
+    mode: "no_cors",
   };
 
   const response = await fetch(url, options);
@@ -61,6 +62,7 @@ const checkoutSuccess = async () => {
       "Content-Type": "application/json",
       "thawani-api-key": "rRQ26GcsZzoEhbrP2HZvLYDbn9C9et",
     },
+    mode: "no_cors",
   };
 
   let url = `https://uatcheckout.thawani.om/api/v1/checkout/session/${global.session_id}`;
@@ -134,12 +136,13 @@ const booking = async () => {
       body: `{"client_reference_id":"123412","mode":"payment","products":[{"name":"product 1","quantity":1,"unit_amount":${
         newPrice * 1000
       }}],"success_url":"https://acacdemy.vercel.app/success-charge","cancel_url":"https://acacdemy.vercel.app/fail-charge","metadata":{"Customer name":"somename","order id":0}}`,
+      mode: "no_cors",
     };
     const response = await fetch(url, options);
     const data = await response.json();
     if (data.success && data.code === 2004) {
       global.session_id = data.data.session_id;
-      const session=createSession();
+      const session = createSession();
       session.sessionId = global.session_id;
       session.save();
     } else {
@@ -212,6 +215,7 @@ const bookingSuccess = async () => {
       "Content-Type": "application/json",
       "thawani-api-key": "rRQ26GcsZzoEhbrP2HZvLYDbn9C9et",
     },
+    mode: "no_cors",
   };
 
   let url = `https://uatcheckout.thawani.om/api/v1/checkout/session/${global.session_id}`;
@@ -223,10 +227,10 @@ const bookingSuccess = async () => {
   }
 
   const session = await Session.findOne({
-    where:{
+    where: {
       sessionId: global.session_id,
-    }
-  })
+    },
+  });
 
   session.isPaid = true;
   session.save();
