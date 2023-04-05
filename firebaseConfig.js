7f6ca3d4aa1bbbdbb4e20f5760e187756ee2b546
shathakh <shathaqudaih17@gmail.com>
2023-04-05 14:14:32 +0300

    create firebase config

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+const firebase = require("firebase");
+const firebaseConfig = {
+  apiKey: "AIzaSyCnbE44feutaPpuJ4TSZ3v_e8W7vW1FOtM",
+  authDomain: "moalemy-88619.firebaseapp.com",
+  projectId: "moalemy-88619",
+  storageBucket: "moalemy-88619.appspot.com",
+  messagingSenderId: "447167686068",
+  appId: "1:447167686068:web:3e1d290da21df58cfd7ee1",
+  measurementId: "G-WWDR5ZT2F0"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const User = db.collection("Users");
+module.exports = User;
