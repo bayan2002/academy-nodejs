1c85ca3c2ad5bc54d3a6a447277a85bd4db4d8cb
shathakh <shathaqudaih17@gmail.com>
2023-04-06 14:36:40 +0300

    send email after sign up

--- controllers/student.js
+++ controllers/student.js
@@ -162,7 +162,7 @@ const signPassword = async (req, res) => {
     to: email,
     subject: "Moalemy platform: Account Creation Successful!",
     html: `<div>Welcome, <br>Thank you so much for taking time to joining us </b>
-    Dear ${name}, We are delighted to inform you that your account has been successfully created.<br>
+    We are delighted to inform you that your account has been successfully created.<br>
     Congratulations on taking the first step towards experiencing our website<br><br>
     Thank you for choosing our services. We look forward to providing you with an exceptional experience.
     Good luck,<br>
