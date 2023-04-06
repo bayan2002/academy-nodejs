ce4b8795305167b30be0e97d4b70c7a61a2d2113
shathakh <shathaqudaih17@gmail.com>
2023-04-06 13:04:42 +0300

    add database url for firebase

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,7 +3,8 @@ const admin = require('firebase-admin');
 const serviceAccount = require('./moaley1-firebase-adminsdk-f0je6-cca4b93de4.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://moaley1-default-rtdb.firebaseio.com/'
 });
 
 const registrationToken = '';
