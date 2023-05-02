5036ad97cf90fdad50c31344baa6eb7d79058803
IbrahemAtef <ibrahim.nemer.1999@gmail.com>
2023-05-02 14:45:24 +0300

    send email changed

--- middlewares/sendEmail.js
+++ middlewares/sendEmail.js
@@ -1,26 +1,25 @@
-const nodemailer=  require('nodemailer');
-const dotenv =require('dotenv');
+const nodemailer = require("nodemailer");
+const dotenv = require("dotenv");
 
 dotenv.config();
 const sendEmail = (mailOptions) => {
   const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
+    host: "moalime.com",
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.APP_EMAIL,
       pass: process.env.APP_EMAIL_PASSWORD,
     },
   });
 
-
   transporter.sendMail(mailOptions, (error) => {
     if (error) {
       console.log(error);
     } else {
-      return '';
+      return "";
     }
   });
 };
 
-module.exports=  sendEmail;
+module.exports = sendEmail;
